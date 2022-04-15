0296c7d9365a8987f933a9d0c07cb99cd9bc9687
Aof <siravich18794@gmail.com>
2022-04-15 15:00:36 +0700

    Setup project

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+// Routes
+const coworking = require("./routes/co-working");
+const auth = require("./routes/auth");
+const reservations = require("./routes/reservation");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+//Connect to database
+connectDB();
+
+//Body parser
+app.use(express.json());
+
+//Cookie parser
+app.use(cookieParser());
+
+//Sanitize data
+app.use(mongoSanitize());
+
+//Set security headers
+app.use(helmet());
+
+//Prevent XSS attacks
+app.use(xss());
+
+//Rate Limiting
+const limiter = rateLimit({
+  windowsMs: 10 * 60 * 1000,
+  max: 1000,
+});
+app.use(limiter);
+
+//Prevent http param pollutions
+app.use(hpp());
+
+//Enable CORS
+app.use(cors());
+
+app.use("/coworking", coworking);
+app.use("/auth", auth);
+app.use("/reservation", reservations);
+const PORT = process.env.PORT | 5000;
+
+const server = app.listen(
+  PORT,
+  console.log("Server running in", process.env.NODE_ENV, "mode on port", PORT)
+);
+
+process.on("unhandleRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
