7a99a6075a434db95f4fc0252fa2ec283469d7b6
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2022-04-22 19:05:17 +0700

    Add CoworkingSpace model

---
+++ models/CoworkingSpace.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const CoworkingSpaceSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, 'Please add a name'],
+			unique: true,
+			trim: true,
+			maxlength: [50, 'Name can not be more than 50 characters'],
+		},
+		address: {
+			type: String,
+			required: [true, 'Please add an address'],
+		},
+		opentime: {
+			type: String,
+			required: [true, 'Please specify the open time'],
+			match: [/[0-1]\d:[0-5]\d [apAP][mM]/, 'Invalid time format'],
+		},
+		closetime: {
+			type: String,
+			required: [true, 'Please specify the close time'],
+			match: [/[0-1]\d:[0-5]\d [apAP][mM]/, 'Invalid time format'],
+		},
+		tel: {
+			type: String,
+		},
+	},
+	{
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true },
+	}
+);
+
+//Cascade delete appointments when a hospital is deleted
+CoWorkingSpaceSchema.pre('remove', async function (next) {
+	console.log(`Reservations being removed from co-working space ${this._id}`);
+	await this.model('Reservation').deleteMany({ coworking: this._id });
+	next();
+});
+
+//Reverse populate with virtuals
+CoWorkingSpaceSchema.virtual('reservations', {
+	ref: 'Reservation', // ref to Appointment Schema
+	localField: '_id',
+	foreignField: 'coworking',
+	justOne: false,
+});
+
+module.exports = mongoose.model('CoworkingSpace', CoWorkingSpaceSchema);
